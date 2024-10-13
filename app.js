fa9d6b2cfa29cdf565fe18e9a86a0932b629d15f
JACM701 <josuechucmedina980@gmail.com>
2024-10-13 15:18:39 -0600

    Actualizacion

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const multer = require ('multer');
-const path = require ('path');
+const multer = require('multer');
+const path = require('path');
 const app = express();
 const bookController = require('./controllers/bookController');
 
-//Configurar Multer para guardar umagenes en la carpeta 'public/uploads'
+// Configurar Multer para guardar imágenes en la carpeta 'public/uploads'
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/uploads');
+        const uploadPath = path.join(__dirname, 'public/uploads');
+        cb(null, uploadPath); // Ruta para guardar las imágenes
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname)); //Nombre Unico
+        const uniqueName = Date.now() + path.extname(file.originalname);
+        cb(null, uniqueName); // Genera un nombre único para la imagen
     }
-    
 });
 
-// Definir la variable 'upload' para manejar la subida de archivos
-const upload = multer({ storage: storage }); // Aquí se define 'upload'
+// Definir el middleware para manejar la subida de archivos
+const upload = multer({ storage: storage });
 
-// Servir archivos estáticos desde la carpeta 'public'
+// Servir archivos estáticos desde la carpeta 'public' para acceder a las imágenes
 app.use(express.static('public'));
 
 // Middleware de Morgan para logging
@@ -33,11 +34,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Rutas CRUD
 app.get('/books', bookController.getAllBooks);
 app.get('/books/:id', bookController.getBookById); // Buscar un libro por ID
-app.post('/books', upload.single('imagen'), bookController.addBook); // Añadir middleware 'upload.single' para subir imágenes
-app.put('/books/:id', upload.single('imagen'), bookController.updateBook); // Añadir multer aquí
+app.post('/books', upload.single('imagen'), bookController.addBook); // Añadir middleware para subir imágenes
+app.put('/books/:id', upload.single('imagen'), bookController.updateBook); // Actualizar libro con imagen
 app.delete('/books/:id', bookController.deleteBook);
 
-
+// Puerto del servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
