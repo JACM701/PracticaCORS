beb106118949338076f6f8a93bc9d285a8d4e72c
JACM701 <josuechucmedina980@gmail.com>
2024-10-28 00:00:53 -0600

    Revan

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,29 +3,25 @@
 const authService = require('../services/authService');
 
 // Registro de usuario
-exports.register = async (req, res) => {
+exports.register = (req, res) => {
     const { username, password } = req.body;
 
     try {
-        const newUser = await authService.createUser(username, password);
+        const newUser = authService.createUser(username, password);
         res.status(201).json({ message: 'Usuario creado', user: newUser });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        res.status(400).json({ message: error.message }); // Manejo de errores
     }
 };
 
 // Iniciar sesión
-exports.login = async (req, res) => {
+exports.login = (req, res) => {
     const { username, password } = req.body;
-    try {
-        const token = await authService.authenticateUser(username, password);
+    const token = authService.authenticateUser(username, password);
 
-        if (!token) {
-            return res.status(401).json({ message: 'Usuario o contraseña incorrectos' });
-        }
-
-        res.json({ auth: true, token });
-    } catch (error) {
-        res.status(500).json({ message: 'Error en la autenticación' });
+    if (!token) {
+        return res.status(401).json({ message: 'Usuario o contraseña incorrectos' });
     }
+
+    res.json({ auth: true, token });
 };
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,16 @@
 // middlewares/authMiddleware.js
 const jwt = require('jsonwebtoken');
-const secretKey = process.env.JWT_SECRET || 'SueñitosTieneHambreTodoElTiempo';
+const secretKey = 'SueñitosTieneHambreTodoElTiempo'; // Cambia esto por una clave más segura y mantenla en un lugar seguro
 
 const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1]; // Obtener el token
 
-    if (!token) return res.status(401).json({ message: 'Token no proporcionado' });
+    if (!token) return res.sendStatus(401); // Si no hay token, devuelve 401
 
     jwt.verify(token, secretKey, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Token inválido' });
-        req.user = user;
-        next();
+        if (err) return res.sendStatus(403); // Si el token es inválido, devuelve 403
+        req.user = user; // Guarda la información del usuario en la solicitud
+        next(); // Continua a la siguiente función
     });
 };
 
