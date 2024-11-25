f3e7f68760f13133d1d1e9ce8394881c50539627
JACM701 <josuechucmedina980@gmail.com>
2024-11-25 10:42:05 -0600

    Si

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,27 +1,31 @@
 // middlewares/authMiddleware.js
-const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'SueñitosTieneHambreTodoElTiempo'; // Usa el mismo que en la creación de tokens
-
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.header('Authorization');
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Token de acceso no proporcionado' });
-  }
-
-  const token = authHeader.split(' ')[1]; // Obtén el token después de "Bearer"
-  if (!token) {
-    return res.status(401).json({ message: 'Token de acceso no proporcionado' });
-  }
-
-  try {
-    const verified = jwt.verify(token, SECRET_KEY); // Valida el token
-    req.user = verified; // Asigna la información del usuario (id, role) al request
-    next();
-  } catch (error) {
-    return res.status(403).json({ message: 'Token no válido' });
-  }
-};
-
+    const authHeader = req.header('Authorization');
+    console.log("Header Authorization:", authHeader);
+  
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      console.log("Token no proporcionado o formato incorrecto");
+      return res.status(401).json({ message: 'Token de acceso no proporcionado' });
+    }
+  
+    const token = authHeader.split(' ')[1];
+    console.log("Token extraído:", token);
+    
+    if (!token) {
+      console.log("Token vacío");
+      return res.status(401).json({ message: 'Token de acceso no proporcionado' });
+    }
+  
+    try {
+      const verified = jwt.verify(token, SECRET_KEY); // Verifica el token
+      console.log("Token verificado con éxito:", verified);
+      req.user = verified; // Asigna la información del usuario al request
+      next();
+    } catch (error) {
+      console.log("Error al verificar el token:", error);
+      return res.status(403).json({ message: 'Token no válido', error: error.message });
+    }
+  };
+  
 module.exports = authenticateToken;
 
