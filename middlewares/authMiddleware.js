e3287373cc4fce9ba16ab4d9362e1da5b6250f6a
JACM701 <josuechucmedina980@gmail.com>
2024-11-25 08:45:43 -0600

    AYUDA

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,27 @@
 // middlewares/authMiddleware.js
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'SueñitosTieneHambreTodoElTiempo';
+const SECRET_KEY = 'SueñitosTieneHambreTodoElTiempo'; // Usa el mismo que en la creación de tokens
 
 const authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization')?.split(' ')[1];
+  const authHeader = req.header('Authorization');
 
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Token de acceso no proporcionado' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Obtén el token después de "Bearer"
   if (!token) {
     return res.status(401).json({ message: 'Token de acceso no proporcionado' });
   }
 
   try {
-    const verified = jwt.verify(token, SECRET_KEY);
-    req.user = verified; // Esto debería contener { id, role }
+    const verified = jwt.verify(token, SECRET_KEY); // Valida el token
+    req.user = verified; // Asigna la información del usuario (id, role) al request
     next();
   } catch (error) {
-    res.status(403).json({ message: 'Token no válido' });
+    return res.status(403).json({ message: 'Token no válido' });
   }
 };
 
 module.exports = authenticateToken;
+
