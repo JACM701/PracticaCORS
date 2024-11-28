29634b887bc6ce37c1c113ec59a7ada3c2004894
JACM701 <josuechucmedina980@gmail.com>
2024-11-27 22:14:42 -0600

    ?

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -5,10 +5,20 @@ const bookController = require('../controllers/bookController');
 const authenticateToken = require('../middlewares/authMiddleware');
 const upload = require('../middlewares/uploadMiddleware'); // Asumiendo que tienes un middleware para manejar la subida de archivos
 
-router.get('/', bookController.getAllBooks);
+// GET all books (no authentication needed)
+router.get('/', bookController.getAllBooks); 
+
+// GET book by ID (authentication required)
 router.get('/:id', authenticateToken, bookController.getBookById);
+
+// POST new book (authentication required)
 router.post('/', authenticateToken, upload.single('imagen'), bookController.addBook);
+
+// PUT update book (authentication required)
 router.put('/:id', authenticateToken, upload.single('imagen'), bookController.updateBook);
+
+// DELETE book (authentication required)
 router.delete('/:id', authenticateToken, bookController.deleteBook);
 
 module.exports = router;
+
